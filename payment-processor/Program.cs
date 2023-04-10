78d840229525ccf6d1dc26b570094744c946b6d6
Matheus-Lara <matheusodelara@gmail.com>
2023-04-09 23:42:09 -0300

    env: adiciona dotnet minimal api para processamento de pagamentos

---
+++ payment-processor/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/")
+    {
+        context.Response.Redirect("/swagger");
+    }
+    else
+    {
+        await next();
+    }
+});
+
+app.MapGet("/health-check", () => "API Health!");
+
+app.Run();
